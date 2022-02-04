822df01fd414559a19a5e5f9ddcca1d14b6faa34
Teri11 <augustineteri157@gmail.com>
2022-02-04 20:53:39 +0300

    added logic to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes: Quote[]=[
-    {id:1, name:'Education is the key to success'},
-    {id:2,name:'Teach a man how to fish'},
-    {id:3,name:'Respect to man is the least art of courtesy'},
-    {id:4,name:'Discpline maketh a man'},
-    
-
-  ]; 
+ 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[]=[
+    {id:1, name:'Education is the key to success'},
+    {id:2,name:'Teach a man how to fish'},
+    {id:3,name:'Respect to man is the least art of courtesy'},
+    {id:4,name:'Discpline maketh a man'},
+    
+
+  ]; 
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
