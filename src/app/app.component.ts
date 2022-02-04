8ba130795af4e0b7264001c02dd3f7be692735fc
Teri11 <augustineteri157@gmail.com>
2022-02-04 17:24:56 +0300

    added a loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes = string[];
+
+  constructor(){
+    this.quotes=['The early bird catches the warm','No man is an island','Respect is a two way-traffic']
+  }
+  
 }
