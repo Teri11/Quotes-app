3bff225ca06a2ef70f8cc3d3082a6a1716e22666
Teri11 <augustineteri157@gmail.com>
2022-02-05 08:24:30 +0300

    added varible author

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   showDescription:boolean;
-  constructor(public topic:string,public name:string,public description:string){
+  constructor(public topic:string,public name:string,public description:string,public author:string){
     this.showDescription = false;
   }
  
