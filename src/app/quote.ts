86289e90eeb422272f6220670d24deb3821a80b8
Teri11 <augustineteri157@gmail.com>
2022-02-05 11:07:29 +0300

    added pipes and modified

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   showDescription:boolean;
-  constructor(public topic:string,public name:string,public description:string,public author:string){
+  constructor(public topic:string,public name:string,public description:string,public completeDate:Date,public author:string){
     this.showDescription = false;
   }
  
