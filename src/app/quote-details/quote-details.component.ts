89fdf7e91bedaa86e8fd8162f75fc90d3973494e
Teri11 <augustineteri157@gmail.com>
2022-02-05 15:27:45 +0300

    added quote delete functionality button

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -11,7 +11,7 @@ export class QuoteDetailsComponent implements OnInit {
   @Input() quote!: Quote;
   @Output() isComplete = new EventEmitter<boolean>();
 
-  quoteComplete(complete:boolean){
+  quoteDelete(complete:boolean){
     this.isComplete.emit(complete);
   }
 
