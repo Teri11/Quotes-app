1e9ca68cc95c3a4b261b30198a6ae96549d39f75
Teri11 <augustineteri157@gmail.com>
2022-02-05 08:25:18 +0300

    modified authors var

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,14 +7,16 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[]=[
-    new Quote('Quotes','“Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.”',' – Robert Frost'),
-    new Quote('Quotes','“We must not allow other people’s limited perceptions to define us.”',' –  Virginia Satir'),
-    new Quote('Quotes','“Wise men speak because they have something to say; fools because they have to say something.”',' –  Plato'),
-    new Quote('Quotes','“If you cannot do great things, do small things in a great way.”',' –   Napoleon Hill'),
+    new Quote('Quotes','“Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.”',' Teri','– Robert Frost'),
+    new Quote('Quotes','“We must not allow other people’s limited perceptions to define us.”',' Teri','–  Virginia Satir'),
+    new Quote('Quotes','“Wise men speak because they have something to say; fools because they have to say something.”',' Teri','– Plato'),
+    new Quote('Quotes','“If you cannot do great things, do small things in a great way.”',' Teri','– Napoleon Hill'),
     
 
   ]; 
-
+toggleDetails(index:number){
+  this.quotes[index].showDescription = !this.quotes[index].showDescription;
+}
   constructor() { }
 
   ngOnInit() {
